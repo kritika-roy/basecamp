d9c45d2cb5b3afbc262daacfc63f09ba45d7303f
Kritika Roy <roykritika2202@gmail.com>
2026-08-15 01:12:33 +0530

    finish up image upload and thumbnail virtual

--- seeds/index.js
+++ seeds/index.js
@@ -29,11 +29,11 @@ const seedDB = async () => {
             images: [
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    filename: 'basecamp/ahfnenvca4tha00h2ubt'
                 },
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
-                    filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
+                    filename: 'basecamp/ruyoaxgf72nzpi4y6cdi'
                 }
             ]
         })

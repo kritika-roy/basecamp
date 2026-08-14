494819fd2411b0fff6e61bd6b991b07184fcfad1
Kritika Roy <roykritika2202@gmail.com>
2026-08-14 20:44:10 +0530

    basic authorization

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const { places, descriptors } = require('./seedHelper');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/basecamp'); 
+mongoose.connect("mongodb://127.0.0.1:27017/basecamp");
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -13,21 +14,24 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++){
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5f5c330c2cd79d538f2c66d9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
-            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum excepturi quae rem, non magni modi esse unde tempora incidunt velit a consequatur consequuntur? Voluptas, sint dicta autem sapiente veniam adipisci!',
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
         await camp.save();
     }
 }
+
 seedDB().then(() => {
     mongoose.connection.close();
-});
+})
